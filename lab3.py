3e50c51b9df279caed72d1c34396a6bb92b7d958
Zikry Danial <zikrydanial@graduate.utm.my>
2024-11-17 15:45:32 +0800

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,1 +1,18 @@
+import requests
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import streamlit as st
 
+
+match_score=1
+miss_score=-1
+gap_penalty=-2
+
+def initialMatrix (Sequence1,Sequence2):
+    
+    row=len(Sequence1)+1
+    column=len(Sequence2)+1
+    matrix=[[0]*(column+1) for_in range(row+1)] #intialize for matrix dimensions (m+1)*(n+1)
+    
+    return initialMatrix
